d3f9bdef8ff0d093eaf6e26af50ea905ab53fdec
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-04-06 07:28:23 +0200

    Match at end of titles
    * helm-emms.el (helm-emms-files-modifier): Do it.

--- helm-emms.el
+++ helm-emms.el
@@ -252,7 +252,7 @@ Returns nil when no music files are found."
                            ((and str
                                  (guard (and playing
                                              (string-match-p
-                                              (regexp-quote playing) str))))
+                                              (concat (regexp-quote playing) "\\'") str))))
                             (propertize str 'face 'emms-browser-track-face))
                            (str (propertize str 'face 'helm-emms-playlist)))
                          (cdr i))
