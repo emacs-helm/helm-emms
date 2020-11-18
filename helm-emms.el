b9fdf06fdbfb37ea81506852c6e2e368cbcbcde6
Daniel Borchmann <daniel@algebra20.de>
2020-11-18 11:33:48 +0100

    Add commentary section for helm-emms
    This is displayed by `describe-package` and thus also indirectly in
    `list-packages`.  Having a commentary thus should help users to understand what
    this package is about.

--- helm-emms.el
+++ helm-emms.el
@@ -21,6 +21,14 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; This package provides a basic helm interface to emms, the Emacs Multimedia
+;; System.  This interface is accessible via the interactive `helm-emms' command
+;; and provides direct access to music directories, individual music files, and
+;; EMMS streams.  See the `helm-emms' customization group for available
+;; customizations, or simply call `helm-emms' to get started.
+
 ;;; Code:
 
 (require 'cl-lib)
